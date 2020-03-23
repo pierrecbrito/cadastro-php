bd4773164c00d4da1ae01257564ccdc3c7230ec7
Pierre Brito <carlospierre07@gmail.com>
2020-03-23 13:35:25 -0300

    Definição dos diretórios CSS's

--- index.php
+++ index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro PHP</title>
+
+    <link rel="stylesheet" href="./assets/css/normalize.css">
+    <link rel="stylesheet" href="./assets/css/container.css">
 </head>
 <body>
-    
+    <main class="container">
+    </main>
 </body>
 </html>
