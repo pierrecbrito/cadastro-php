ae4e775e9805c6d691e997a8729e60bc2ff74f9d
Pierre Brito <carlospierre07@gmail.com>
2020-03-23 13:43:27 -0300

    Definição do container

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
     <title>Cadastro PHP</title>
 
     <link rel="stylesheet" href="./assets/css/normalize.css">
+    <link rel="stylesheet" href="./assets/css/body.css">
     <link rel="stylesheet" href="./assets/css/container.css">
 </head>
 <body>
     <main class="container">
+        <h1 class="container__titulo">Cadastro Simples com PHP</h1>
     </main>
 </body>
 </html>
